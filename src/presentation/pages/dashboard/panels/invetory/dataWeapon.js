f32baa15529cae400bef5cf1bb66938d5281dd02
PierrisKevin <krandy018@gmail.com>
2024-05-05 07:44:11 +0300

    a peu pres fin du inventor

---
+++ src/presentation/pages/dashboard/panels/invetory/dataWeapon.js
@@ -0,0 +1,161 @@
+export const weaponData = [
+    {
+      name: 'Weapon',
+      speed: 56,
+      cadence: 34,
+      precision: 78,
+      image: '/images/1.webp'
+   }
+   ,
+   {
+      name: 'Weapon',
+      speed: 23,
+      cadence: 87,
+      precision: 45,
+      image: '/images/7.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 89,
+      cadence: 12,
+      precision: 64,
+      image: '/images/5.webp'
+   }
+   ,
+   {
+      name: 'Weapon',
+      speed: 67,
+      cadence: 55,
+      precision: 91,
+      image: '/images/2.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 44,
+      cadence: 78,
+      precision: 33,
+      image: '/images/1.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 12,
+      cadence: 89,
+      precision: 77,
+      image: '/images/1.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 90,
+      cadence: 45,
+      precision: 22,
+      image: '/images/4.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 76,
+      cadence: 23,
+      precision: 56,
+      image: '/images/6.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 32,
+      cadence: 67,
+      precision: 88,
+      image: '/images/1.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 55,
+      cadence: 34,
+      precision: 79,
+      image: '/images/2.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 78,
+      cadence: 12,
+      precision: 46,
+      image: '/images/5.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 43,
+      cadence: 88,
+      precision: 21,
+      image: '/images/7.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 67,
+      cadence: 45,
+      precision: 90,
+      image: '/images/3.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 22,
+      cadence: 79,
+      precision: 55,
+      image: '/images/1.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 88,
+      cadence: 56,
+      precision: 35,
+      image: '/images/4.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 34,
+      cadence: 33,
+      precision: 68,
+      image: '/images/6.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 66,
+      cadence: 78,
+      precision: 11,
+      image: '/images/5.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 45,
+      cadence: 11,
+      precision: 77,
+      image: '/images/1.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 79,
+      cadence: 22,
+      precision: 54,
+      image: '/images/2.webp'
+   },
+   
+   {
+      name: 'Weapon',
+      speed: 11,
+      cadence: 54,
+      precision: 89,
+      image: '/images/3.webp'
+   }
+  ];
